bccf1dfd378a90ddc4ebb74e63e2e2969d53ed84
Masayoshi Honda <masayoshi.honda@gmail.com>
2019-08-18 09:30:14 +0900

    first commit

---
+++ chat.js
@@ -0,0 +1,82 @@
+class Chat {
+    constructor(el) {
+        this.el = el;
+        const db = firebase.firestore();
+        this.collection = db.collection('messages');
+        this.messagesDiv = el.querySelector('.messages');
+
+        this.listenCollection();
+        this.listenForm();
+    }
+
+    listenCollection() {
+        this.collection.orderBy('createdAt').onSnapshot({
+            includeMetadataChanges: true,
+        }, snapshot => {
+            snapshot.docChanges().filter(change => {
+                return (change.type === 'added' && change.doc.data().createdAt)
+                    || change.type === 'modified'
+            }).forEach(change => {
+                this.onAdd(change.doc.data());
+            });
+        }, error => {
+            alert(`Error listening collection: ${error}`);
+        });
+    }
+
+    onAdd(data) {
+        const containerDiv = document.createElement('div');
+        const userDiv = document.createElement('div');
+        const timeDiv = document.createElement('div');
+        const pre = document.createElement('pre');
+
+        containerDiv.classList.add('message');
+        userDiv.classList.add('user');
+        timeDiv.classList.add('time');
+
+        userDiv.textContent = data.user || '(匿名)';
+        timeDiv.textContent = this.formatDate(data.createdAt.seconds);
+        pre.textContent = data.message;
+        
+        containerDiv.appendChild(userDiv);
+        containerDiv.appendChild(timeDiv);
+        containerDiv.appendChild(pre);
+        this.messagesDiv.insertBefore(containerDiv, this.messagesDiv.firstChild);
+    }
+
+    formatDate(seconds) {
+        const d = new Date(seconds * 1000);
+        return d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDay()
+            + ' ' + d.getHours()
+            + ':' + d.getMinutes().toString().padStart(2, '0')
+            + ':' + d.getSeconds().toString().padStart(2, '0');
+    }
+
+    listenForm() {
+        const form = this.el.querySelector('.input-form');
+        const userNameArea = form.querySelector('.user');
+        const messageArea = form.querySelector('.message');
+
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            const [user, message] = [userNameArea.value, messageArea.value];
+            if (message.length === 0) {
+                return;
+            }
+
+            this.add(user, message).then(docRef => {
+                messageArea.value = '';
+            }).catch(error => {
+                alert(`Error adding a message: ${error}`);
+            });
+        })
+    }
+
+    add(user, message) {
+        return this.collection.add({
+            user,
+            message,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+    }
+}
